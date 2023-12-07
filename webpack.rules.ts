3a60bf6ed80b1c962b08d7205039ab285f041c61
huanNguyen2204 <huan.nc2204@sinhvien.hoasen.edu.vn>
2023-12-08 05:24:16 +0700

    add babel

--- webpack.rules.ts
+++ webpack.rules.ts
@@ -28,4 +28,14 @@ export const rules: Required<ModuleOptions>['rules'] = [
       },
     },
   },
+  // {
+  //   test: /\.(ts|js)x?$/i,
+  //   exclude: /node_modules/,
+  //   use: {
+  //     loader: 'babel-loader',
+  //     options: {
+  //       presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+  //     },
+  //   },
+  // },
 ];
